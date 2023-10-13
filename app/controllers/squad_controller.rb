77c1310d4f5e8fc393e36dd354a75dcc04402644
Andreas Hansen <ndrshnsn@gmail.com>
2023-10-13 18:14:14 -0300

    feat: add tactic map with club players

--- app/controllers/squad_controller.rb
+++ app/controllers/squad_controller.rb
@@ -4,9 +4,13 @@ class SquadController < ApplicationController
   breadcrumb "squad.main", :squad_path, match: :exact, frame: "main_frame"
 
   def index
+    @club_players = User.getTeamPlayers(current_user.id, @season.id).includes(:player_season)
+    @team_strength = Club.getTeamStrength(current_user.id, @season.id)
+    @team_overall_avg = @team_strength == 0 ? 0 : @team_strength/@club_players.size
   end
 
   def set_controller_vars
     @season = Season.find(session[:season])
+    @club = User.getClub(current_user.id, @season.id)
   end
 end
