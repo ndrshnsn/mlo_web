b51a8a366f56312567e875914eb546e9403041a8
Andreas Hansen <ndrshnsn@gmail.com>
2024-07-19 16:59:09 -0300

    feat: fix email helper image path

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,6 +1,6 @@
 module MailerHelper
   def email_image_tag(image, **options)
-    attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    attachments[image] = File.read(Rails.root.join("app/frontend/images/#{image}"))
     image_tag attachments[image].url, **options
   end
 end
