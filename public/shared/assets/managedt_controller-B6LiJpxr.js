084d34f08207e5085da78bc77fdcd99fa1d4ec60
Andreas Hansen <ndrshnsn@gmail.com>
2024-07-19 14:16:22 -0300

    feat: move vite to another folder and remove asset_sync

--- public/shared/assets/managedt_controller-B6LiJpxr.js
+++
@@ -1,2 +0,0 @@
-import{C as e}from"./application-BzDBbQCT.js";class r extends e{clear_state(){sessionStorage.setItem("clearDTState",!0)}}export{r as default};
-//# sourceMappingURL=managedt_controller-B6LiJpxr.js.map
