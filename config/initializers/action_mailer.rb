c8aa646f72a23239483a1e39f5e64a14ad9316d6
Andreas Hansen <ndrshnsn@gmail.com>
2024-07-20 23:46:30 -0300

    fix: action mailer config

--- config/initializers/action_mailer.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'gmail.com',
-  user_name: Rails.application.credentials.dig(:sendmail, :username),
-  password: Rails.application.credentials.dig(:sendmail, :password),
-  authentication: :login,
-  enable_starttls_auto: true
-}
