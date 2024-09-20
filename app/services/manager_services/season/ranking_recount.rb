8acca7510884f7cecd44a6551cf3614ecb705299
Andreas Hansen <ndrshnsn@gmail.com>
2024-09-20 12:04:13 -0300

    fix: ranking show

--- app/services/manager_services/season/ranking_recount.rb
+++ app/services/manager_services/season/ranking_recount.rb
@@ -25,13 +25,34 @@ class ManagerServices::Season::RankingRecount < ApplicationService
       championship_club_awards = ClubAward.joins(:championship_award).where(championship_awards: { championship_id: championship.id } )
 
       championship_club_awards.each do |club_award|
+        description = nil
+        case club_award.source.award_type
+        when "goaler"
+          goalers = Championship.getGoalers(championship, 2)
+          if goalers.length == 1 || ( goalers.size > 1 && ( goalers.first.goals > goalers.second.goals ) )
+            description = goalers.first.id
+          end
+        when "assister"
+          assister = Championship.getAssisters(championship, 2)
+          if assister.length == 1 || ( assister.size > 1 && assister.first.assists > assister.second.assists )
+            description = assister.first.id
+          end
+        when "bestplayer"
+          if championship.preferences["match_best_player"] == "on"
+            bestPlayer = Championship.getBestPlayer(championship, 2)
+            if bestPlayer.length == 1 || ( bestPlayer.size > 1 && bestPlayer.first.bestplayer > bestPlayer.second.bestplayer )
+              description = bestPlayer.first.id
+            end
+          end
+        end 
+        
         ranking = Ranking.new(
           season_id: @season.id,
           club_id: club_award.club_id,
           operation: "award",
           points: club_award.source.award.ranking,
           source: club_award.source,
-          description: nil
+          description: description
         )
         return handle_error(ranking&.error) unless ranking.save!
       end
@@ -44,3 +65,5 @@ class ManagerServices::Season::RankingRecount < ApplicationService
     OpenStruct.new(success?: false, season: season, error: error)
   end
 end
+
+
